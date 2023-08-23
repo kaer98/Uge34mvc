5ddbccc073905a0a8f27ce51aecef824ca003674
kaer98 <56426532+kaer98@users.noreply.github.com>
2023-08-23 09:28:04 +0200

    add project

---
+++ uge34mvc/uge34mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uge34mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
